f2518fffd0b50b0d6da44ec30c184b13d0c03e1e
Majedul Hoque <rumanbsl@gmail.com>
2019-09-26 23:02:15 +0300

    cleanup

--- main.js
+++ main.js
@@ -1,22 +1,18 @@
-const { Board, Sensor, Pin } = require("johnny-five");
+const { Board, Pin } = require("johnny-five");
 const board = new Board({ repl: false });
 
 function boardFuncs() {
   const self = board;
-
-  self.pinMode(3, Pin.PWM); // ->Sets digital pin 3 as analog output
-
-  // analogRead always reads Analog PIN, in this case "A3"
-  self.analogRead(3, function (voltage) {
-    const outputVoltage = (255 / 1023) * voltage;
-    console.log(outputVoltage)
-    this.analogWrite(3, outputVoltage)
-  });
-
-
-  self.on("exit", function () {
-    console.log("\nexiting")
+  //reading "A3" analog pin
+  self.analogRead(3, function (rawVoltage) {
+    let outputVoltage = (255 / 1023) * rawVoltage;
+    self.pinMode(3, Pin.PWM); // digital pin 3 outputs analog signal
+    // now writing dynamic voltage value, effecting dimming and brightening
+    self.analogWrite(3, outputVoltage); // write happens on digital PIN, there are no analog OUT in arduino
   });
 }
 
 board.on("ready", boardFuncs);
+board.on("exit", function () {
+  console.log("\nexiting")
+});
